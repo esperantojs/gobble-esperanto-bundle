41ce34d1d2414cad09699c2a16cd9e75248ede83
Rich Harris <richard.a.harris@gmail.com>
2014-11-14 10:51:13 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var methods = {
+	amd: 'toAmd',
+	cjs: 'toCjs',
+	umd: 'toUmd'
+};
+
+module.exports = function esperantoBundle ( inputdir, outputdir, options, callback ) {
+	var fs = require( 'fs' ),
+		path = require( 'path' ),
+		esperanto = require( 'esperanto' ),
+		method = methods[ options.type ] || 'toAmd';
+
+	if ( !options.entry ) {
+		throw new Error( "The gobble-esperanto-bundle config must specify an entry module (e.g. `{ entry: 'main' }`)" );
+	}
+
+	options.base = path.join( inputdir, options.base || '' );
+
+	esperanto.bundle( options ).then( function ( bundle ) {
+		var result = bundle[ method ]( options ),
+			dest = path.join( outputdir, ( options.dest || options.entry ) + '.js' );
+
+		fs.writeFile( dest, result, callback );
+	}).catch( callback );
+};
